4957f8a2dcc6b43371b5bf2ef2600282646c2fff
SynthCyraX <SynthCyraX@gmail.com>
2023-04-09 13:19:38 +0200

    Changed innerHTML to innerText

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,5 @@
 function checkPasswords() {
+    let passwordMatch = document.getElementById("password-match");
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirm-password").value;
     let button = document.getElementById("submit-button");
@@ -6,32 +7,35 @@ function checkPasswords() {
     let confirmPasswordInput = document.getElementById("confirm-password");
   
     if ( (password != confirmPassword ) || ( password || confirmPassword) === "") {
-      document.getElementById("password-match").innerHTML = "Passwords doesn't match";
-  
-      button.disabled = true
-  
-      passwordInput.classList.remove("green")
-      passwordInput.classList.add("red")
-  
-      confirmPasswordInput.classList.remove("green")
-      confirmPasswordInput.classList.add("red")
-  
-      button.classList.remove("validButton")
-      button.classList.add("invalidButton")
+        passwordMatch.innerText = "Passwords doesn't match";
+        passwordMatch.classList.remove("validPassword")
+        passwordMatch.classList.add("invalidPassword")
+
+        button.disabled = true
+        
+        passwordInput.classList.remove("green")
+        passwordInput.classList.add("red")
+        
+        confirmPasswordInput.classList.remove("green")
+        confirmPasswordInput.classList.add("red")
+        
+        button.classList.remove("validButton")
+        button.classList.add("invalidButton")
   
     }else{
-      document.getElementById("password-match").innerHTML = "Passwords match.";
+        passwordMatch.innerText = "Passwords match.";
+        passwordMatch.classList.remove("invalidPassword")
+        passwordMatch.classList.add("validPassword")
+        button.disabled = false
   
-      button.disabled = false
-  
-      passwordInput.classList.remove("red")
-      passwordInput.classList.add("green")
-  
-      confirmPasswordInput.classList.remove("red")
-      confirmPasswordInput.classList.add("green")
-  
-      button.classList.remove("invalidButton")
-      button.classList.add("validButton")
+        passwordInput.classList.remove("red")
+        passwordInput.classList.add("green")
+        
+        confirmPasswordInput.classList.remove("red")
+        confirmPasswordInput.classList.add("green")
+        
+        button.classList.remove("invalidButton")
+        button.classList.add("validButton")
   
       }
     }
